bd0f5dc848fb20827dbead34f9ddea8c96e7116b
Mustafa Özbal <99390718+mustafaozbal2@users.noreply.github.com>
2023-01-29 10:18:42 +0300

    Add files via upload

---
+++ İf else/if-else.php
@@ -0,0 +1,19 @@
+<?php
+ 
+//PHP if else if örneği: Sayının İşaretini Öğrenme
+$sayi=-20;
+ 
+if($sayi > 0)
+{
+    echo $sayi. " Pozitif";
+}
+else if($sayi < 0)
+{
+    echo $sayi. " Negatif";
+}
+else
+{
+    echo $sayi. " Sıfır(0)";   
+}
+ 
+?>
